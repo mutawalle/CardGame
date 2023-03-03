2ed963b79805230a63bfbae670378d37c0612d01
Zaki <zakiamanullah215@gmail.com>
2023-03-03 19:21:56 +0700

    gak sengaja

--- src/card/abstractValue.hpp
+++ src/card/abstractValue.hpp
@@ -5,11 +5,21 @@
 class AbstractValue{
     public:
         virtual double value() const = 0;
-        bool operator>(const AbstractValue& other){
+        bool operator>(const AbstractValue& other) {
             return this->value() > other.value();
         }
-        bool operator<(const AbstractValue& other);
-        bool operator==(const AbstractValue& other);
+        bool operator<(const AbstractValue& other) {
+            return this->value() < other.value();
+        }
+        bool operator<=(const AbstractValue& other) {
+            return this->value() <= other.value();
+        }
+        bool operator>=(const AbstractValue& other) {
+            return this->value() >= other.value();
+        }
+        bool operator==(const AbstractValue& other) {
+            return this->value() == other.value();
+        }
 };
 
 
