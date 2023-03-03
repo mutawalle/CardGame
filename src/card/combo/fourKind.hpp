358c519273b660c8adaea2ebfb685954bcd35190
Vieri Fajar Firdaus <16521294@mahasiswa.itb.ac.id>
2023-03-03 21:40:02 +0700

    combo

---
+++ src/card/combo/fourKind.hpp
@@ -0,0 +1,17 @@
+#include "../abstractValue.hpp"
+#include "../card.hpp"
+#include <vector>
+
+class fourKind :public AbstractValue {
+    private:
+        vector<Card> cards;
+    public:
+        fourKind(vector<Card> cards){
+            this->cards=cards;
+        }
+        double getValue(){
+            int highest_number=cards[0].getNumber();
+            
+            return 9.2+(highest_number-1)*0.1;
+        };
+};
