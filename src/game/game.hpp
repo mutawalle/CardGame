795c8024672657f8c1a938b6fae5d8c4f93a9450
mutawalle <mutawally.nawwar@jatis.com>
2023-03-12 01:20:17 +0700

    feat: add some in game, gameState

--- src/game/game.hpp
+++ src/game/game.hpp
@@ -1,17 +1,61 @@
 #ifndef GAME_H
 #define GAME_H
+#include "gameState.hpp"
+
+#include <vector>
+#include <string>
+
+#include "../command/command.hpp"
+
+using namespace std;
+
+vector<string> abilities = {"re-roll", "quadruple", "quarter", "reverse", "swap", "switch", "abilityless"}; 
 
 class Game {
     private:
-        // gameState
-        // ronde keberapa (int)
-        // urutan (list of int)
-        // isFinish (boolean) untuk menandakan apakah sudah ada pemenangnya
-        // tableCard (list of card)
+        GameState gameState;
+        int round;
+        bool isFinish;
     public:
-        void start();
-        void splitCards();
-        void ronde();
+        Game(){
+            this->gameState = GameState();
+            this->round = 1;
+            this->isFinish = false;
+        }
+
+        void start(){
+            while(!isFinish){
+                ronde();
+            }
+        }
+
+        void splitCards(){
+            int j = 1;
+            for(i=52;i>38;i--){
+                int randVal = rand() % i;
+                gameState.players[j%7].cards.insert(gameState.deckCard[randVal]);
+                gameState.deckCard.er
+            }
+        }
+
+        void splitAbilities(){
+            for(int i=7;i>0;i--){
+                int randVal = rand() % i;
+                gameState.players[8-i].ability = abilities[randVal];
+                abilities.erase(randVal);
+            }
+        }
+
+        void ronde(){
+            if(this->round==1){
+                splitCards();
+            }else if(this->round==2){
+                splitAbilities();
+            }
+
+            Command command;
+            command.run();
+        }
 };
 
 #endif
--- src/game/gameState.hpp
+++ src/game/gameState.hpp
@@ -1,13 +1,49 @@
 #ifndef GAMESTATE_HPP
 #define GAMESTATE_HPP
 
+#include <vector>
+#include "../player/player.hpp"
+#include "../card/deckCard.hpp"
+#include "../card/tableCard.hpp"
+
 class GameState {
     private:
-        // listPLayer (Player) list player 
-        // deckCard (list of card) list kartu yang belum dibagi
-        // poin (long int) poin saat ini
-        // giliran (int)
-        // canUseAbility (boolean) untuk menandakan apakah ability dimatikan atau tidak 
+        vector<Player> players;
+        DeckCard deckCard;
+        TableCard tableCard;
+        long long int point;
+        vector<int> order;
+        int turn;
+        bool canUseAbility;
+    public:
+        GameState(){
+            players = {Player(), Player(), Player(), Player(), Player(), Player(), Player()};
+            this->point = 64;
+            this->order = {0,1,2,3,4,5,6};
+            this->turn = 0;
+            this->canUseAbility = false;
+        }
+
+        GameState(long long int point, vector<int> order, int turn, bool canUseAbility){
+            this->point = point;
+            this->order = order;
+            this->turn = turn;
+            this->canUseAbility = canUseAbility;
+        }
+
+        GameState& operator=(const GameState& other){
+            if(this != &other){
+                this->point = other.point;
+                this->order = other.order;
+                this->turn = other.turn;
+                this->canUseAbility = other.canUseAbility;
+            }
+            return *this;
+        }
+
+        setPlayers(vector<Player>){
+            return this->
+        }
 };
 
 #endif
