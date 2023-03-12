ccc252e35608d55a2567580a063d84420d92562c
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-03-12 22:49:16 +0700

    player beres(belum debug/test)

--- src/command/command.hpp
+++ src/command/command.hpp
@@ -2,7 +2,7 @@
 #define COMMAND_HPP
 
 #include <bits/stdc++.h>
-#include "../game/gameState.hpp"
+// #include "../game/gameState.hpp"
 #include "commandException.hpp"
 #include "../ability/Action.hpp"
 
@@ -39,7 +39,7 @@ class Command {
             while(!valid){
                 try{
                     commandValidation();
-                    abillityValidation();
+                    // abillityValidation();
                     valid = true;
                 }
                 catch(commandException err){
@@ -53,87 +53,87 @@ class Command {
         void setGameState(GameState gameState){
             this->gameState = gameState;
         }
-        void abillityValidation(){
-            Action action(gameState);
-            switch (command)
-            {
-            case "next":
-                action.Next();
-                break;
-            case "half":
-                action.half();
-                break;
-            case "double":
-                action.Double();
-                break;
-            case "re-roll":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "re-roll"){
-                    action.reRoll();
-                }
-                else{
-                    commandException err(" ", "re-roll");
-                    throw err;
-                }
-                break;
-            case "quadruple":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "quadruple"){
-                    action.Quadruple();
-                }
-                else{
-                    commandException err(" ", "quadruple");
-                    throw err;
-                }
-                break;
-            case "quater":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "qurter"){
-                    action.Quarter();
-                }
-                else{
-                    commandException err(" ", "quarter");
-                    throw err;
-                }
-                break;
-            case "reverseDirection":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "reverseDirection"){
-                    action.reverseDirection();
-                }
-                else{
-                    commandException err(" ", "reverseDirection");
-                    throw err;
-                }
-                break;
-            case "swap-card":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "swap-card"){
-                    action.swapCard();
-                }
-                else{
-                    commandException err(" ", "swap-card");
-                    throw err;
-                }
-                break;
-            case "switch":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "switch"){
-                    action.switch();
-                }
-                else{
-                    commandException err(" ", "switch");
-                    throw err;
-                }
-                break;
-            case "abillityless":
-                if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "abilityLess"){
-                    action.abillityLess();
-                }
-                else{
-                    commandException err(" ", "abillityLess");
-                    throw err;
-                }
-                break;
+        // void abillityValidation(){
+        //     Action action(gameState);
+        //     switch (command)
+        //     {
+        //     case "next":
+        //         action.Next();
+        //         break;
+        //     case "half":
+        //         action.half();
+        //         break;
+        //     case "double":
+        //         action.Double();
+        //         break;
+        //     case "re-roll":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "re-roll"){
+        //             action.reRoll();
+        //         }
+        //         else{
+        //             commandException err(" ", "re-roll");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "quadruple":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "quadruple"){
+        //             action.Quadruple();
+        //         }
+        //         else{
+        //             commandException err(" ", "quadruple");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "quater":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "qurter"){
+        //             action.Quarter();
+        //         }
+        //         else{
+        //             commandException err(" ", "quarter");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "reverseDirection":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "reverseDirection"){
+        //             action.reverseDirection();
+        //         }
+        //         else{
+        //             commandException err(" ", "reverseDirection");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "swap-card":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "swap-card"){
+        //             action.swapCard();
+        //         }
+        //         else{
+        //             commandException err(" ", "swap-card");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "switch":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "switch"){
+        //             action.switch();
+        //         }
+        //         else{
+        //             commandException err(" ", "switch");
+        //             throw err;
+        //         }
+        //         break;
+        //     case "abillityless":
+        //         if(gameState.getPlayer(gameState.getTurn()).getAbillity() == "abilityLess"){
+        //             action.abillityLess();
+        //         }
+        //         else{
+        //             commandException err(" ", "abillityLess");
+        //             throw err;
+        //         }
+        //         break;
 
-            default:
-                break;
-            }
-        }
+        //     default:
+        //         break;
+        //     }
+        // }
         
 };
 
--- src/player/player.hpp
+++ src/player/player.hpp
@@ -21,7 +21,7 @@ class Player {
         void bubbleSort() {
             for (int i = 0; i < playerCard.getCard().size() - 1; i++) {
                 for (int j = 0; j < playerCard.getCard().size()- i - 1; j++) {
-                    if (playerCard.getCard()[j].getNumber() > playerCard.getCard()[j+1].getNumber()) {
+                    if (playerCard.getCard()[j].getValue() > playerCard.getCard()[j+1].getValue()) {
                         Card temp = playerCard.getCard()[j];
                         playerCard.getCard()[j] = playerCard.getCard()[j+1];
                         playerCard.getCard()[j+1] = temp;
@@ -60,25 +60,11 @@ class Player {
 
         vector<Card> threeOfKind(){
             vector<Card> Three;
-            int countThree = 0;
-            for(int i = 0; i < playerCard.getCard().size() -1 ; i++){
-                for(int j = i+1; j < playerCard.getCard().size(); j++){
-                    if(playerCard.getCard()[i].getNumber() == playerCard.getCard()[j].getNumber()){
-                        for(int k = j+1; k < playerCard.getCard().size(); k++){
-                            if(playerCard.getCard()[i].getNumber() == playerCard.getCard()[k].getNumber()){
-                                countThree ++;
-                                Three.push_back(playerCard.getCard()[i]);
-                                Three.push_back(playerCard.getCard()[j]);
-                                Three.push_back(playerCard.getCard()[k]);
-                                break;
-                            }
-                        }
-                    }
-                    if(countThree == 1){
-                        break;
-                    }
-                }
-                if(countThree == 1){
+            for(int i = playerCard.getCardListLength()-1; i>=2; i--){
+                if(playerCard.getCard()[i].getNumber() == playerCard.getCard()[i-1].getNumber() && playerCard.getCard()[i].getNumber() == playerCard.getCard()[i-2].getNumber()){
+                    Three.push_back(playerCard.getCard()[i]);
+                    Three.push_back(playerCard.getCard()[i-1]);
+                    Three.push_back(playerCard.getCard()[i-2]);
                     break;
                 }
             }
@@ -86,34 +72,173 @@ class Player {
         }
 
         vector<Card> straight (){
-            // //vector sudah terurut
-            // int index = 0;
-            // vector<Card> straightVec;
-            // for(int i = 0; i < playerCard.getCard().size(); i++){
-            //     bool str = false;
-            //     int count = 0;
-            //     for(int j = i+1; j< playerCard.getCard().size(); j++){
-            //         if(playerCard.getCard()[i].getNumber() - i == playerCard.getCard()[j].getNumber() - j){
-            //             count ++;
-            //         }
-            //         if(count == 5){
-            //             str = true;
-            //             break;
-            //         }
-            //     }
-
-            // }
-            // if(str){
-            //     for(int i = index; i<index+4; i++){
-            //         straightVec.push_back(playerCard.getCard()[i]);
-            //     }
-            // }
-            // return straightVec;
+            //vector sudah terurut
+            int index = 0;
+            vector<Card> straightVec;
+            bool str = false;
+            for(int i = 0; i <= playerCard.getCard().size()-5; i++){
+                int count = 1;
+                for(int j = i+1; j < i+4; j++){
+                    if(playerCard.getCard()[i].getNumber() - i == playerCard.getCard()[j].getNumber() - j){
+                        count ++;
+                    }
+                    if(count == 5){
+                        str = true;
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if(str){
+                for(int i = index ; i<index+5; i++){
+                    straightVec.push_back(playerCard.getCard()[i]);
+                }
+            }
+            
+            return straightVec;
         }
-        double value(){
-            // if(isFullHouse){
-            //     value = 
-            // }
+
+        vector<Card> Flush (){
+            //Total kartu ada 7, jadi maksimal hanya ada 1 kombinasi flush
+            bool isFlush = false;
+            vector<Card> FlushCard;
+            string color;
+            for(int i = 0; i<playerCard.getCardListLength(); i++){
+                int count = 0;
+                for(int j = i+1; j < playerCard.getCardListLength(); j++){
+                    if(playerCard.getCard()[i].getColor() == playerCard.getCard()[j].getColor()){
+                        count ++;
+                    }
+                    if(count == 5){
+                        isFlush = true;
+                        color = playerCard.getCard()[i].getColor();
+                    }
+                }
+            }
+            if(isFlush){
+                int i = playerCard.getCardListLength() - 1;
+                //list sudah terurut, untuk memaksimalkan combo dipilih kartu dengan nilai paling besar
+                while(FlushCard.size() < 5){
+                    if(playerCard.getCard()[i].getColor() == color){
+                        FlushCard.push_back(playerCard.getCard()[i]);
+                    }
+                    i--;
+                }
+            }
+            return FlushCard;
+        }
+        
+        vector<Card> FullHouse (){
+            vector<Card> temp;
+            int count2Card = 0;
+            int number1 = 0;
+            for(int i = playerCard.getCardListLength()-1; i>=2; i--){
+                if(playerCard.getCard()[i].getNumber() == playerCard.getCard()[i-1].getNumber() && playerCard.getCard()[i].getNumber() == playerCard.getCard()[i-2].getNumber()){
+                    number1 = playerCard.getCard()[i].getNumber();
+                    break;
+                }
+            }
+            if(number1 == 0){
+                return temp;
+            }
+            else{
+                for(int i = 0; i < playerCard.getCardListLength(); i++){
+                    if(playerCard.getCard()[i].getNumber() == number1){
+                        temp.push_back(playerCard.getCard()[i]);
+                    }
+                }
+
+                for(int i = playerCard.getCardListLength()-1; i>=1; i--){
+                    if(playerCard.getCard()[i].getNumber() == playerCard.getCard()[i-1].getNumber() && playerCard.getCard()[i].getNumber() != number1){
+                        temp.push_back(playerCard.getCard()[i]);
+                        temp.push_back(playerCard.getCard()[i-1]);
+                        break;
+                    }
+                }
+                if(temp.size() == 5){
+                    return temp;
+                }
+                else{
+                    return {};
+                }
+            }
+            
+        }
+
+        vector<Card> FourOfKind (){
+            vector<Card> FOKcard;
+            for(int i = playerCard.getCardListLength()-1; i>= 3; i--){
+                bool found = true;
+                for(int j = 1; j < 4; j++){
+                    if(playerCard.getCard()[i].getNumber() != playerCard.getCard()[i-j].getNumber()){
+                        found = false;
+                    }
+                }
+                if(found){
+                    for(int k = i; k>=i-3; k--){
+                        FOKcard.push_back(playerCard.getCard()[k]);
+                    }
+                    break;
+                }
+            }
+            return FOKcard;
+        }
+
+        vector<Card> straightFlush(){
+            vector<Card> SFCard;
+            bool str = false;
+            int index = 0;
+            for(int i = 0; i <= playerCard.getCard().size()-5; i++){
+                int count = 1;
+                for(int j = i+1; j < i+4; j++){
+                    if(playerCard.getCard()[i].getValue() - i == playerCard.getCard()[j].getValue() - j){
+                        count ++;
+                    }
+                    if(count == 5){
+                        str = true;
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if(str){
+                for(int i = index ; i<index+5; i++){
+                    SFCard.push_back(playerCard.getCard()[i]);
+                }
+            }
+            return SFCard;
+        }
+        vector<Card> value(){
+            bubbleSort();
+            if(straightFlush().size() == 5){
+                return straightFlush();
+            }
+            else if(FourOfKind().size() == 4){
+                return FourOfKind();
+            }
+            else if(FullHouse().size() == 5){
+                return FullHouse();
+            }
+            else if(Flush().size() == 5 ){
+                return Flush();
+            }
+            else if(straight().size() == 5){
+                return straight();
+            }
+            else if(threeOfKind().size() == 3){
+                return threeOfKind();
+            }
+            else if(PairCard().size() == 4){
+                return PairCard();
+            }
+            else if(PairCard().size() == 2){
+                return PairCard();
+            }
+            else{
+                vector<Card> high;
+                high.push_back(getHighCard());
+                return high;
+            }
         }
 };
 
