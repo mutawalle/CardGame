3a16cbb7cd583c83c15f399b84f3a8cb6cf31ac6
Ulung32 <13521122@mahasiswa.itb.ac.id>
2023-03-15 12:49:17 +0700

    feat action

--- src/ability/half.hpp
+++ src/ability/half.hpp
@@ -11,8 +11,14 @@ public:
     Half() {}
     Half (GameState gameState) : Action(gameState) {}
     void DoAction() {
+        cout<< gameState.players.at(gameState.order.at(this->gameState.turn)).getName() << " melakukan HALF"<<endl;
         if(this->gameState.point>1){
+            cout<< "point turun dari " << gameState.point;
             this->gameState.point/=2;
+            cout<< " menjadi " << gameState.point<< endl;
+        }
+        else if(this->gameState.point==1){
+            cout << "Sayangnya poin hadiah sudah bernilai 1. Poin hadiah tidak berubah.. Giliran dilanjut!"<<endl;
         }
         this->gameState.turn++;
         this->gameState.turn %= 7;
